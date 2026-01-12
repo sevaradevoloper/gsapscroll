7227dd3b12da3fa7d97010b4290f4bccfc9931f4
sevara128@icloud.com <sevara128@icloud.com>
2026-01-13 00:53:16 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ gsap.registerPlugin(ScrollTrigger);
 
 // 1. O'ng tarafdagi rasm (bg-right) - Chapga surilishi
 gsap.to(".bg-right", {
-    x: -150, // Chapga 150px suriladi
+    x: -100,
+    y: 700, // Chapga 150px suriladi
+     rotation: -55,
     scrollTrigger: {
         trigger: ".hero",
         start: "top top", 
@@ -13,8 +15,9 @@ gsap.to(".bg-right", {
 
 // 2. Chap tarafdagi rasm (bg-left) - Pastga va O'ngga surilishi
 gsap.to(".bg-left", {
-    y: 100,  // Pastga 100px
-    x: 100,  // O'ngga 100px
+    y:650,  // Pastga 100px
+    x: 0,  // O'ngga 100px
+    rotation: -155,
     scrollTrigger: {
         trigger: ".calculate",
         start: "top bottom", // Calculate qismi ko'rinishni boshlaganda
@@ -26,6 +29,14 @@ gsap.to(".bg-left", {
 // Hero matnlari uchun oddiy kirib kelish animatsiyasi
 gsap.from(".hero__container > *", {
     y: 50,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.2,
+    ease: "power2.out"
+});
+
+gsap.from(".calculate__container > *", {
+    y: 0,
     opacity: 0,
     duration: 1,
     stagger: 0.2,
